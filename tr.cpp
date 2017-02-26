72b7efefc88fc743058a3e497dbda9086bb0abd7
FKZ PC <FKZ PC>
2017-02-26 20:02:46 +0700

    Implementation of input data validation functions for correctness

--- tr.cpp
+++ tr.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int S, D;
+int S, D, flag;
+int CorF (int, int);
 
 int main()
 {
@@ -13,4 +14,20 @@ int main()
          cin >> S;     
      cout << "Amount of days: ";
          cin >> D;
+         
+         CorF (S, D);
+         
+         system ("PAUSE");
+         
 }
+
+       int CorF (int, int)
+           { 
+            if ((S<10000) || ((D<-1) || (D>365))) 
+            {
+            flag=1;
+            system("cls");
+            cout << "The entered data is not correct!" << endl << "Check the entered data!" << endl << "The contribution rate of 10 thousand rubles and up to 365 days!" << endl;
+            }
+           }         
+           
