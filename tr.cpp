373d74956f4f6a6e3cfcdbe922811e6dae5ae590
FKZ PC <FKZ PC>
2017-02-26 19:37:23 +0700

    Data input

---
+++ tr.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int S, D;
+
+int main()
+{
+     setlocale(LC_ALL, "RUS");
+    
+     cout << "Сумма вклада: ";
+         cin >> S;     
+     cout << "Количество дней вклада: ";
+         cin >> D;
+}
