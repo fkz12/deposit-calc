43742e1729e694f73b93790a84344972610f2dcb
fkz12 <ip611s23@awp09.csc.local>
2017-03-15 12:33:16 +0700

    Fix for terminal

--- tr.cpp
+++ tr.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-int S, D, flag;
+int S, D, flag, t;
 int CorF (int, int);
-int DayF (int, int);
+void DayF (int, int);
 
 int main()
 {
@@ -34,31 +34,33 @@ int main()
             system("cls");
             cout << "The entered data is not correct!" << endl << "Check the entered data!" << endl << "The contribution rate of 10 thousand rubles and up to 365 days!" << endl;
             }
+	return 1;
            }         
            
-    int DayF (int, int)
+    void DayF (int, int)
 {
         if (S<100000)
         {
-        if (D > -1 && D <= 30)
-           cout << "Income= " << S*0.9;
-        if (D > 30 && D <= 120)
-             cout << "Income= " << S*1.02;
-        if (D > 120 && D <= 240)
-            cout << "Income= " << S*1.06;
-        if (D > 241 && D <= 365)
-            cout << "Income= " << S*1.12;
+        if (D > -1 && D <= 30) { 
+           cout << "Income= " << S*0.9; }
+        if (D > 30 && D <= 120) {
+             cout << "Income= " << S*1.02; }
+        if (D > 120 && D <= 240) {
+            cout << "Income= " << S*1.06; }
+        if (D > 241 && D <= 365) {
+            cout << "Income= " << S*1.12; }
         }
         else
         {
-        if (D > -1 && D <= 30)
-            cout<< "Income= " << S*0.9;
-        if (D > 30 && D <= 120)
-             cout<< "Income= " << S*1.03;
-        if (D > 120 && D <= 240)
-             cout<< "Income= " << S*1.08;
-        if (D > 241 & D <= 365)
-            cout<< "Income= " << S*1.15;
+        if (D > -1 && D <= 30)  {
+            cout<< "Income= " << S*0.9; }
+
+        if (D > 30 && D <= 120) {
+             cout<< "Income= " << S*1.03; }
+        if (D > 120 && D <= 240){
+             cout<< "Income= " << S*1.08; }
+        if (D > 241 && D <= 365){
+            cout<< "Income= " << S*1.15 ; }
         }
 }
 
