30a9bd08059dea7f0bd17d76f74d3c9080b0aafd
AzGround <aleksejj.anisimov@mail.ru>
2017-04-26 05:40:01 +0700

    Upgrading for lab_8

--- makefile
+++ makefile
@@ -1,10 +1,13 @@
 CC              = gcc
 CFLAGS          = -Wall -Werror
+TEST_FLAGS      = -I src -I thirdparty -Wall -Werror
 EXECUTABLE      = bin/calc
 SRC_BUILD_DIR   = build/src
 SRC_DIR         = src/
+TEST_EXEC       = bin/test
+TEST_OBJ_DIR    = build/test
 
-all: makedir compile
+all: makedir compile test
 
 makedir: 
 	mkdir -p bin build/src
@@ -18,8 +21,26 @@ $(SRC_BUILD_DIR)/main.o: $(SRC_DIR)/main.c
 $(SRC_BUILD_DIR)/deposit.o: $(SRC_DIR)/deposit.c
 	$(CC) $(CFLAGS) -c $(SRC_DIR)/deposit.c -o $(SRC_BUILD_DIR)/deposit.o
 
+
+test: mktest test-comp
+
+mktest:
+	mkdir -p bin build/test
+
+test-comp: $(TEST_OBJ_DIR)/main.o $(TEST_OBJ_DIR)/deposit_test.o $(TEST_OBJ_DIR)/validation_test.o $(SRC_BUILD_DIR)/deposit.o
+	$(CC) $(TEST_OBJ_DIR)/main.o $(TEST_OBJ_DIR)/deposit_test.o $(TEST_OBJ_DIR)/validation_test.o $(SRC_BUILD_DIR)/deposit.o -o $(TEST_EXEC)
+
+$(TEST_OBJ_DIR)/main.o: test/main.c
+	$(CC) $(TEST_FLAGS) -c test/main.c -o $(TEST_OBJ_DIR)/main.o
+
+$(TEST_OBJ_DIR)/deposit_test.o: test/deposit_test.c
+	$(CC) $(TEST_FLAGS) -c test/deposit_test.c -o $(TEST_OBJ_DIR)/deposit_test.o
+
+$(TEST_OBJ_DIR)/validation_test.o: test/validation_test.c
+	$(CC) $(TEST_FLAGS) -c test/validation_test.c -o $(TEST_OBJ_DIR)/validation_test.o
+
 .c.o:
 	$(CC) $(CFLAGS) $< -o $@
-.PHONY: clean
+
 clean:
 	rm -rf build/ bin/
---
+++ test/deposit_test.c
@@ -0,0 +1,59 @@
+#include "deposit.h"
+#include "ctest.h"
+
+CTEST(PenaltyTest, penaltyAppliedTenPercentOK)
+{
+    const int result = perform(20, 1000);
+    const int expected = 900;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(LessBlockTests, goodDataPass102)
+{
+    const int result = perform(60, 1000);
+    const int expected = 1020;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(LessBlockTests, goodDataPass106)
+{
+    const int result = perform(200, 1000);
+    const int expected = 1060;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(LessBlockTests, goodDataPass112)
+{
+    const int result = perform(300, 1000);
+    const int expected = 1120;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(MoreBlockTests, goodDataPass103)
+{
+    const int result = perform(100, 200000);
+    const int expected = 206000;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(MoreBlockTests, goodDataPass108)
+{
+    const int result = perform(200, 200000);
+    const int expected = 216000;
+    ASSERT_EQUAL(expected, result);
+}
+
+CTEST(MoreBlockTests, goodDataPass115)
+{
+    const int result = perform(300, 200000);
+    const int expected = 229999;
+    ASSERT_EQUAL(expected, result);
+}
+/*
+CTEST(FailTests, failTest01)
+{
+    const int result = perform(122, 1000);
+    const int expected = 1020;
+    ASSERT_EQUAL(expected, result);
+}
+*/
